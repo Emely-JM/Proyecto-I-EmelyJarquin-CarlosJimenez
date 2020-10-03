25cdef61b5f0e16edbf994e0030715e2127fc344
EMELY DEL SOCORRO JARQUIN MONGE <ejarquinm@est.utn.ac.cr>
2020-10-02 18:25:53 -0600

    Interfaz del menu de administradores

--- Matricula/Matricula.gui/MenuAdmin.cs
+++ Matricula/Matricula.gui/MenuAdmin.cs
@@ -12,37 +12,49 @@ namespace Matricula.gui
 {
     public partial class MenuAdmin : Form
     {
+        bool admin1;
+        string u;
+
         /// <summary>
         /// Imprime mensajes sobre los botones con tooltip
         /// </summary>
         private void mensajes()
         {
-            this.toolTip1.SetToolTip(btnCRUDAdmin,"Mantenimiento de administradores");
+            this.toolTip1.SetToolTip(btnCRUDAdmin, "Mantenimiento de administradores");
             this.toolTip1.SetToolTip(btnCRUDCarreras, "Mantenimiento de carreras");
             this.toolTip1.SetToolTip(btnCRUDMateria, "Mantenimiento de materias");
+            this.toolTip1.SetToolTip(btnCRUDPersonas, "Mantenimiento de personas");
         }
 
-        public MenuAdmin()
+        public MenuAdmin(bool admin, string usu)
         {
             InitializeComponent();
             mensajes();
+            admin1 = admin;
+            u = usu;
         }
 
         private void btnCRUDAdmin_Click(object sender, EventArgs e)
         {
-            MantenimientoAdmin frm = new MantenimientoAdmin();
+            MantenimientoAdmin frm = new MantenimientoAdmin(admin1);
             frm.ShowDialog();
         }
 
         private void btnCRUDCarreras_Click(object sender, EventArgs e)
         {
-            MantenimientoCarrera frm = new MantenimientoCarrera();
+            MantenimientoCarrera frm = new MantenimientoCarrera(admin1);
             frm.ShowDialog();
         }
 
         private void btnCRUDMateria_Click(object sender, EventArgs e)
         {
-            MantenimientoMateria frm = new MantenimientoMateria();
+            MantenimientoMateria frm = new MantenimientoMateria(admin1);
+            frm.ShowDialog();
+        }
+
+        private void btnCRUDPersonas_Click(object sender, EventArgs e)
+        {
+            MantenimientoPersona frm = new MantenimientoPersona(admin1, u);
             frm.ShowDialog();
         }
     }
--- Matricula/Matricula.gui/Properties/Resources.Designer.cs
+++ Matricula/Matricula.gui/Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace Matricula.gui.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_user_groups_125px {
+            get {
+                object obj = ResourceManager.GetObject("icons8_user_groups_125px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap LoginForm1 {
             get {
                 object obj = ResourceManager.GetObject("LoginForm1", resourceCulture);
