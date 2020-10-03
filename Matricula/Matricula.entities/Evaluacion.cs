eff6bf829c67023d6535aa28c32c296556a1869a
EMELY DEL SOCORRO JARQUIN MONGE <ejarquinm@est.utn.ac.cr>
2020-10-02 23:27:42 -0600

    Clase Evaluacion con los métodos gets,sets,toString y constructores.

---
+++ Matricula/Matricula.entities/Evaluacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matricula.entities
+{
+    public class Evaluacion
+    {
+        public string idEvalucion { get; set; }
+        public string descripcion { get; set; }
+        public string idEstudiante { get; set; }
+        public string estado { get; set; }
+        public DateTime fechaEvaluacion { get; set; }
+
+        public Evaluacion()
+        {
+        }
+
+        public Evaluacion(string idEvalucion, string descripcion, string idEstudiante, string estado, DateTime fechaEvaluacion)
+        {
+            this.idEvalucion = idEvalucion;
+            this.descripcion = descripcion;
+            this.idEstudiante = idEstudiante;
+            this.estado = estado;
+            this.fechaEvaluacion = fechaEvaluacion;
+        }
+
+        public string toString()
+        {
+            return idEvalucion + "," + descripcion + "," + idEstudiante + "," + estado + "," + fechaEvaluacion;
+        }
+    }
+}
