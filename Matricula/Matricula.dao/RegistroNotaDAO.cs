aaa1da4981bbc132e54af11edea5262248e44418
EMELY DEL SOCORRO JARQUIN MONGE <ejarquinm@est.utn.ac.cr>
2020-10-04 16:58:18 -0600

    Paso de la clase RegistroNota al paquete de entities

---
+++ Matricula/Matricula.dao/RegistroNotaDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matricula.dao
+{
+    public class RegistroNotaDAO
+    {
+        List<RegistroNota> matriculas = new List<RegistroNota>();
+        private string nombreArchivo = "Registro de notas.txt";
+
+
+    }
+}
