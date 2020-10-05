366a864f2cf0d6d22064cbbdde43ea3c910d5f79
CARLOS MARIO JIMENEZ BARRANTES <cjimenezb@est.utn.ac.cr>
2020-10-04 19:05:53 -0600

    Se crearon nuevamente las ventanas de Mantenimiento de usuario y Edición de usuario por errores de sincronización del Github

--- Matricula/Matricula.gui/FrmUsuario.Designer.cs
+++ Matricula/Matricula.gui/FrmUsuario.Designer.cs
@@ -30,18 +30,19 @@
         {
             this.pnlTitulo = new System.Windows.Forms.Panel();
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
             this.lblCodigo = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabla = new System.Windows.Forms.DataGridView();
             this.txtbCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.txtbTipoUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.chkbActivo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnContrasena = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
-            this.btnAgregar = new System.Windows.Forms.Button();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.pnlTitulo.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlTitulo
@@ -61,17 +62,16 @@
             this.lblTitulo.Location = new System.Drawing.Point(238, 12);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(363, 31);
-            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.TabIndex = 1;
             this.lblTitulo.Text = "Mantenimiento de usuarios";
             // 
-            // txtCodigo
+            // txtBuscar
             // 
-            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCodigo.Location = new System.Drawing.Point(294, 73);
-            this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(250, 31);
-            this.txtCodigo.TabIndex = 1;
-            this.txtCodigo.Tag = "";
+            this.txtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(294, 73);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(250, 31);
+            this.txtBuscar.TabIndex = 1;
             // 
             // lblCodigo
             // 
@@ -80,22 +80,22 @@
             this.lblCodigo.Location = new System.Drawing.Point(202, 76);
             this.lblCodigo.Name = "lblCodigo";
             this.lblCodigo.Size = new System.Drawing.Size(86, 25);
-            this.lblCodigo.TabIndex = 6;
+            this.lblCodigo.TabIndex = 7;
             this.lblCodigo.Text = "Código:";
             // 
-            // dataGridView1
+            // tabla
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tabla.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.txtbCodigo,
             this.txtbTipoUsuario,
             this.chkbActivo});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 114);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(814, 315);
-            this.dataGridView1.TabIndex = 7;
+            this.tabla.Location = new System.Drawing.Point(12, 114);
+            this.tabla.Name = "tabla";
+            this.tabla.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tabla.Size = new System.Drawing.Size(814, 315);
+            this.tabla.TabIndex = 8;
             // 
             // txtbCodigo
             // 
@@ -114,32 +114,45 @@
             this.chkbActivo.HeaderText = "Activo";
             this.chkbActivo.Name = "chkbActivo";
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Image = global::Matricula.gui.Properties.Resources.icons8_plus_25px_1;
+            this.btnAgregar.Location = new System.Drawing.Point(648, 68);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(40, 40);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnContrasena
+            // 
+            this.btnContrasena.Image = global::Matricula.gui.Properties.Resources.icons8_password_25px_1;
+            this.btnContrasena.Location = new System.Drawing.Point(786, 68);
+            this.btnContrasena.Name = "btnContrasena";
+            this.btnContrasena.Size = new System.Drawing.Size(40, 40);
+            this.btnContrasena.TabIndex = 5;
+            this.btnContrasena.UseVisualStyleBackColor = true;
+            // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(786, 68);
+            this.btnEliminar.Image = global::Matricula.gui.Properties.Resources.icons8_minus_25px_1;
+            this.btnEliminar.Location = new System.Drawing.Point(740, 68);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(40, 40);
-            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.TabIndex = 4;
             this.btnEliminar.UseVisualStyleBackColor = true;
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(740, 68);
+            this.btnEditar.Image = global::Matricula.gui.Properties.Resources.icons8_edit_25px_1;
+            this.btnEditar.Location = new System.Drawing.Point(694, 68);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(40, 40);
-            this.btnEditar.TabIndex = 4;
+            this.btnEditar.TabIndex = 3;
             this.btnEditar.UseVisualStyleBackColor = true;
-            // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(694, 68);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(40, 40);
-            this.btnAgregar.TabIndex = 3;
-            this.btnAgregar.UseVisualStyleBackColor = true;
             // 
             // btnBuscar
             // 
+            this.btnBuscar.Image = global::Matricula.gui.Properties.Resources.icons8_search_25px_1;
             this.btnBuscar.Location = new System.Drawing.Point(550, 68);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(40, 40);
@@ -151,19 +164,21 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 441);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tabla);
             this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnContrasena);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnEditar);
-            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtBuscar);
             this.Controls.Add(this.pnlTitulo);
             this.Name = "FrmUsuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Matricula 0.1";
             this.pnlTitulo.ResumeLayout(false);
             this.pnlTitulo.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,13 +188,14 @@
 
         private System.Windows.Forms.Panel pnlTitulo;
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtBuscar;
         private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnContrasena;
+        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Label lblCodigo;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView tabla;
         private System.Windows.Forms.DataGridViewTextBoxColumn txtbCodigo;
         private System.Windows.Forms.DataGridViewTextBoxColumn txtbTipoUsuario;
         private System.Windows.Forms.DataGridViewCheckBoxColumn chkbActivo;
